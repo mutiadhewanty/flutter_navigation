43068ae9c9134a7500163cd77ee93366981bd8f5
mutiadhewanty <mutiadhewanty@gmail.com>
2022-03-20 12:43:42 +0700

    first commit

---
+++ animate_a_widget_across_screens/flutter_animate_widget_across_screens/android/app/src/main/kotlin/com/example/flutter_animate_widget_across_screens/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_animate_widget_across_screens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ animate_a_widget_across_screens/flutter_animate_widget_across_screens/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Transition Demo',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DetailScreen();
+          }));
+        },
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+              'https://tse1.mm.bing.net/th?id=OIP.lngq-49XGPxJZ7SwzClqsQHaFQ&pid=Api&P=0&w=279&h=198'),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+              tag: 'imageHero',
+              child: Image.network(
+                  'https://tse1.mm.bing.net/th?id=OIP.lngq-49XGPxJZ7SwzClqsQHaFQ&pid=Api&P=0&w=279&h=198')),
+        ),
+      ),
+    );
+  }
+}
